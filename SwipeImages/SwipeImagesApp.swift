fd00a5e1b590e12a37e50a4213a49c602621ef1b
MAC <mac@MacBook-Pro.local>
2022-08-27 08:55:58 -0500

    first commit

---
+++ SwipeImages/SwipeImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeImagesApp.swift
+//  SwipeImages
+//
+//  Created by MAC on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
